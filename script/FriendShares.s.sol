7474569198e318fbcc9435e08def2b07ae488b1d
kphed <kpcrypto69@gmail.com>
2023-08-23 17:20:48 -0400

    Add initial FriendShares contract (based on FT V1)

--- script/FriendShares.s.sol
+++ script/FriendShares.s.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.21;
+pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract FriendSharesScript is Script {
     function setUp() public {}
 
     function run() public {
