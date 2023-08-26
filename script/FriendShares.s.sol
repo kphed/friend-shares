a681cccf5c88896f92baeb424daef1db5a43c654
kphed <kpcrypto69@gmail.com>
2023-08-25 21:07:14 -0400

    Add deployment script

--- script/FriendShares.s.sol
+++ script/FriendShares.s.sol
@@ -2,11 +2,17 @@
 pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
+import {FriendShares} from "src/FriendShares.sol";
 
 contract FriendSharesScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        new FriendShares(vm.envAddress("PROTOCOL"));
+
+        vm.stopBroadcast();
     }
 }
